f3cf984ff29342a6a152b1495846d5c25349e3fb
alxsmp <simpsonajb@gmail.com>
2023-05-17 14:07:20 +0100

    Removed Symlinks

--- src/utils/createTimeDeferred.ts
+++ src/utils/createTimeDeferred.ts
@@ -1,1 +1,12 @@
-../../../mimic-3d/src/utils/createTimeDeferred.ts
+export function createTimeDeferred<T>(action: (v: T) => void, intervalMs: number, initialValue: T | undefined = undefined): [() => T | undefined, (v: T) => void] {
+    let last = initialValue
+    let current = last
+
+    setInterval(() => {
+        if (current && current !== last) {
+            last = current
+            action(current)
+        }
+    }, intervalMs)
+    return [() => current, (v: T) => current = v]
+}
--- src/utils/range.ts
+++ src/utils/range.ts
@@ -1,1 +1,3 @@
-../../../mimic-3d/src/utils/range.ts
+export function range(start: number, stop: number, step = 1) : number[] {
+    return Array(Math.ceil((stop - start) / step)).fill(start).map((x, y) => x + y * step)
+}
